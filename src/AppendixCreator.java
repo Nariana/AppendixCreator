35966412165b5db042b94f061e34afef56e1137e
Nariana <nsands@smu.edu>
2016-04-26 22:12:48 -0500

    FINALLY got the regex right for removing punctuation and stop words

--- src/AppendixCreator.java
+++ src/AppendixCreator.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import java.io.*;
 import java.nio.file.*;
@@ -13,18 +15,20 @@ public class AppendixCreator {
 		System.out.println("Welcome to the Appendix Creator. Enter a valid file path to begin.");
 		
 		// read input file path
-		String inpath = s.next();
-		System.out.println("You entered " + inpath);
-		System.out.println("Enter a valid path for the output file you would like to create. (example: \"output.txt\"");
-		
-		// read output file path
-		String outpath = s.next();
-		System.out.println("You entered " + outpath);
+//		String inpath = s.next();
+//		System.out.println("You entered " + inpath);
+//		System.out.println("Enter a valid path for the output file you would like to create. (example: \"output.txt\"");
+//		
+//		// read output file path
+//		//String outpath = s.next();
+//		String outpath = "C:/Users/ysands/Desktop/dummy.txt";
+//		System.out.println("You entered " + outpath);
 		
 
 		
 		// read contents of file into string
-		content = readFile(inpath);
+//		content = readFile(inpath);
+		content = "!!The the cats ran for their life! But I didn't stop it; alligators are my favorite.";
 		// if file has <= 1 character, prompt user to try again
 		if (content.length() <= 1)
 			System.out.println("File looks empty or invalid. Try again.");
@@ -38,7 +42,10 @@ public class AppendixCreator {
 		content = parseTags(content);
 		
 		// parse punctuation and numbers out of string
-		content = parseNonwords(content);
+		content = parseNonWords(content);
+		
+		// parse filler words out of string
+		content = parseStopWords(content);
 		
 	}
 	
@@ -57,15 +64,36 @@ public class AppendixCreator {
 	// removes XML/HTML tags in file
 	public static String parseTags(String content){
 		content = content.replaceAll("<[^>]+>", "");
-		System.out.println(content);
 		return content;
 	}
 	
 	// removes all punctuation and numbers in file
-	public static String parseNonwords(String content){
-		content = content.replaceAll("\\p{Punct}|\\p{Digit}", " ");
-		System.out.println(content);
+	public static String parseNonWords(String content){
+		// removes all digits and punctuation, excluding conjoining apostrophes because they make conjunctions
+		content = content.replaceAll("^\\p{Punct}+|\\W+\\p{Punct}+|\\p{Punct}+\\W+|\\p{Punct}+$"," ").replaceAll("\\p{Punct}&&[^\u0027]|\\p{Digit}", " ");
+		//System.out.println(content);
 		return content;
+	}
+	
+	// removes stop words in file, such as article adjectives
+	public static String parseStopWords(String content){
+		// hard-coded URL with text file for stop words
+		String stopURL = "C:/Users/ysands/workspace/AppendixCreator/stopwords.txt";
+		// read words in file into string
+		String stopWords = readFile(stopURL);
+		
+		// split string into string list w/ stop word at each array
+		String [] s = stopWords.split("\\s+");
+		List<String> stopWordsList = Arrays.asList(s);
+		//System.out.println(stopWordsList);
+		
+		// iterate over list to remove stop words
+		for(int i = 0; i < stopWordsList.size(); i++){
+			content = content.replaceAll("(?i)\\s*\\b"+ stopWordsList.get(i)+"\\b\\s*", " ");
+		}
+		//System.out.println(content);
+		return content;
+		
 	}
 
 }
