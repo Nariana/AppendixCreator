538d8ad6ffed3bb0529c94927bc680499c21e5f0
Nariana <nsands@smu.edu>
2016-04-27 08:54:04 -0500

    separated doc parser implementation into a class

--- src/IndexCreator.java
+++ src/IndexCreator.java
@@ -38,21 +38,23 @@ public class IndexCreator {
 		// close scanner
 		s.close();
 		
+		// initialize document parser to parse file
+		DocumentParser dp = new DocumentParser();
+		
 		// parse XML/HTML out of string
-		content = parseTags(content);
+		content = dp.parseTags(content);
 		
 		// parse punctuation and numbers out of string
-		content = parseNonWords(content);
+		content = dp.parseNonWords(content);
 		
 		// parse filler words out of string
-		content = parseStopWords(content);
+		content = dp.parseStopWords(content);
 		
 		// create list for index now that main words are identified
 		String [] a = content.split("\\s");
 		List<String> index = Arrays.asList(a);
 		
-//		index = stemWords(index);
-		
+		// create database and prompt user
 		JDBC j = new JDBC();
 		j.createDB(index);
 		//j.selectIndex();
@@ -62,67 +64,4 @@ public class IndexCreator {
 		j.closeDB();
 	}
 	
-	// read contents of file into string
-	public static String readFile(String inpath){
-		String message = null;
-		try{
-			message = new String(Files.readAllBytes(Paths.get(inpath)));
-		}
-		catch(IOException e) {
-			message = "."; // return 1 character to trigger trying again
-		}
-		return message;
-	}
-	
-	// removes XML/HTML tags in file
-	public static String parseTags(String content){
-		content = content.replaceAll("<[^>]+>", "");
-		return content;
-	}
-	
-	// removes all punctuation and numbers in file
-	public static String parseNonWords(String content){
-		// removes all digits and punctuation, excluding conjoining apostrophes because they make conjunctions
-		content = content.replaceAll("^\\p{Punct}+|\\W+\\p{Punct}+|\\p{Punct}+\\W+|\\p{Punct}+$"," ").replaceAll("\\p{Punct}&&[^\u0027]|\\p{Digit}", " ");
-		//System.out.println(content);
-		return content;
-	}
-	
-	// removes stop words in file, such as article adjectives
-	public static String parseStopWords(String content){
-		// hard-coded URL with text file for stop words
-		String stopURL = "C:/Users/ysands/workspace/IndexCreator/stopwords.txt";
-		// read words in file into string
-		String stopWords = readFile(stopURL);
-		
-		// split string into string list w/ stop word at each array
-		String [] s = stopWords.split("\\s+");
-		List<String> stopWordsList = Arrays.asList(s);
-		//System.out.println(stopWordsList);
-		
-		// iterate over list to remove stop words
-		for(int i = 0; i < stopWordsList.size(); i++){
-			content = content.replaceAll("(?i)\\s*\\b"+ stopWordsList.get(i)+"\\b\\s*", " ");
-		}
-		
-		// remove single characters
-		//content = content.replaceAll("\\s+[^\\s]{1}\\s+", " ");
-		content = content.replaceAll("^.{1}\\s+|\\s+.{1}\\s+|\\s+.{1}$", " ");
-		//System.out.println(content);
-		
-		return content;
-		
-	}
-	
-	// TODO
-	public static List<String> stemWords(List<String> index){
-		Stemmer s = new Stemmer();
-//		for(int i = 0; i < index.size(); i++){
-//			index.set(i, s.stem(index.get(i)));
-//		}
-//		
-		
-		return index;
-	}
-
 }
